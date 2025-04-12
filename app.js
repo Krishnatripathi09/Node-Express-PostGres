00f2a2bf537fdc80ad06c42fd1a3fa207b91a6a8
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-04-12 20:02:07 +0530

    feat(signIn): Installed bcrypt to encrypt password when a user is created and Created a Log-In API and checked the password and email before Successful Sign-In

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const pool = require("./db.js");
+const bcrypt = require("bcrypt");
 const app = express();
 
 app.use(express.json());
 const PORT = 5000;
 
-app.post("/user", async (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const userInfo = await pool.query(
       `INSERT INTO "Users"("firstName","lastName","email","password") VALUES ($1,$2,$3,$4)`,
-      [firstName, lastName, email, password]
+      [firstName, lastName, email, passwordHash]
     );
 
     res
@@ -19,6 +22,39 @@ app.post("/user", async (req, res) => {
       .send(`Data Inserted SuccessFully ${firstName},${lastName},${email}`);
   } catch (error) {
     console.log(error.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await pool.query(`SELECT * FROM "Users" WHERE email = $1`, [
+      email,
+    ]);
+    if (user.rows.length === 0) {
+      res
+        .status(400)
+        .send("User Not Found With this Email Please Enter Valid Email 🤨");
+    }
+
+    const validUser = await bcrypt.compare(password, user.rows[0].password);
+    if (validUser) {
+      res.status(200).send("Logged-In SuccessFully");
+    } else {
+      res.status(400).send("Invalid Password");
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await pool.query(`Select * from "Users"`);
+    res.status(200).send(user.rows);
+  } catch (err) {
+    console.error(err);
   }
 });
 
